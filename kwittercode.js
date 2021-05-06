bf005e37664ae9edf7cc27fdab3b141a12f167da
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-06 12:20:05 -0400

    Update kwittercode.js

--- kwittercode.js
+++ kwittercode.js
@@ -6,3 +6,16 @@ function addUser() {
 
     window.location = "kwitter_rooms.html"
 }
+
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+    
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"
+    });
+    
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
