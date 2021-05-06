32152a2e4c1219c32ac1afc0b396e56606a61a1e
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-06 12:29:22 -0400

    Update kwittercode.js

--- kwittercode.js
+++ kwittercode.js
@@ -4,7 +4,7 @@ function addUser() {
 
     localStorage.setItem("user_name", user_name);
 
-    window.location = "kwitter_rooms.html"
+    window.location = "kwitter_room.html"
 }
 
 function addRoom()
