04b706cec76a781ed35b80ff7cf45b2b4db096c6
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-06 12:28:23 -0400

    Update kwitter_rooms.js

--- kwitter_rooms.js
+++ kwitter_rooms.js
@@ -16,6 +16,13 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       console.log("Room Name - " + Room_Names);
       row = "<div class='room_name' id="+Room_Names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
       document.getElementById("output").innerHTML += row;
+      
+      function redirectToRoomName(name)
+      {
+      console.log(name);
+      localStorage.setItem("room_name", name);
+            window.location = "kwitter_page.html";
+      }
       //End code
       });});}
 getData();
