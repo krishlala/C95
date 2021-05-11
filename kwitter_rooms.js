2b3b79d47fcd0b828a3d155f12b5332b73eb0e58
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-11 12:19:04 -0400

    Update kwitter_rooms.js

--- kwitter_rooms.js
+++ kwitter_rooms.js
@@ -55,3 +55,12 @@ localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
     window.location = "index.html";
 }
+
+function send()
+{
+ msg = document.getElementById("msg").value:
+ firebase.database().ref(room_name).push({
+  name:user_name,
+  message:msg,
+  like:0
+ });
